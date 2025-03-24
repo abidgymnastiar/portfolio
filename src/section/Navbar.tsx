80d0f28485dccc5bb4f77c31ee1b7754c6cd0034
abidgymnastiar <abidgymnastiar@gmail.com>
2025-03-24 10:22:45 +0700

    fix: menu navbar dan responsive

--- src/section/Navbar.tsx
+++ src/section/Navbar.tsx
@@ -1,28 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MENU_LINKS } from "../utils/Data";
 import { Link } from "react-scroll";
-import { FiMenu, FiX } from "react-icons/fi";
+import { CgMenuMotion } from "react-icons/cg";
+import { CgClose } from "react-icons/cg";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(true);
-//   const toggleMenu = () => {
-//     setIsOpen(!isOpen);
-//   };
-  //   const closeMenu = () => {
-  //     setIsOpen(false);
-  //   };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setIsOpen(true);
+      } else {
+        setIsOpen(false);
+      }
+    };
+
+    handleResize(); 
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <nav className="container mx-auto sticky top-5 z-10">
       <div className="flex items-center justify-between rounded-full bg-white/25 border border-[#fee6cc] backdrop-blur-md m-5 p-3 md:p-0">
         <h1 className="uppercase font-sans font-bold text-3xl">Portfolio</h1>
 
+        {/* Hamburger Icon */}
         <button
-          className="md:hidden p-2"
-          onClick={() => setIsOpen(!isOpen)} // Memanggil setIsOpen langsung
+          className="block md:hidden text-[#333] mr-6 focus:outline-none"
+          onClick={toggleMenu}
         >
-          {isOpen ? <FiX size={24} /> : <FiMenu size={24} />}
+          {isOpen ? (
+            <CgClose className="w-7 h-7" fill="none" />
+          ) : (
+            <CgMenuMotion className="w-7 h-7" fill="none" />
+          )}
         </button>
 
+        {/* Navigation LINK */}
         <ul className={`${isOpen ? "flex" : "hidden"} menu-wrapper`}>
           {MENU_LINKS.map((item) => (
             <li key={item.id}>
@@ -39,7 +62,9 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        <button className="">Hire Me</button>
+        <button className="hidden md:block h-12 text-[15px] font-medium text-white bg-gradient-primary rounded-full px-9 transition-transform duration-300 ease-in-out hover:scale-105">
+          Hire Me
+        </button>
       </div>
     </nav>
   );
