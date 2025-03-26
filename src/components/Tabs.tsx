4137ccc5342fcc551623b096636d3f3dbf56e68b
abidgymnastiar <abidgymnastiar@gmail.com>
2025-03-26 11:41:30 +0700

    feat: animasi menu tabs profesi

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 interface Tab {
   id: string;
   value: string;
@@ -20,15 +22,17 @@ function Tabs({ tabList, activeTab, onChange }: TabsProps) {
     <div className="flex items-center justify-center my-10">
       <div className="bg-[#fff6eb] rounded-full flex">
         {tabList.map((tab) => (
-          <button
+          <motion.button
             key={tab.id}
+            initial={{ opacity: 0.0, scale: 1 }}
+            animate={{ opacity: activeTab === tab.value ? 1 : 0.8, scale: activeTab === tab.value ? 1.05 : 1 }}
             className={`text-xs, md:text-[15px] ${getActiveStyles(
               tab.value
             )} rounded-full px-4 md:px-10 py-[6px] md:py-3`}
             onClick={() => onChange(tab.value)}
           >
             {tab.label}
-          </button>
+          </motion.button>
         ))}
       </div>
     </div>
